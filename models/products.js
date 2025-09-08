bb8669411019940b98a5036122b70cfceab2b4fd
Micaela Svatzky <svatzkymicaela@gmail.com>
2025-09-08 17:45:02 -0300

    added model

--- models/products.js
+++ models/products.js
@@ -2,7 +2,15 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: { type: String },
+  name: { type: String, required: true },
+  category: { type: String, enum: ["Mockup", "Plantilla"], required: true },
+  description: { type: String },
+  format: { type: String },
+  price: { type: Number },
+  tags: [{ type: String }],
+  fileUrl: { type: String },  
+  imageUrl: { type: String }, 
+
 });
 
 export default mongoose.model("Product", productSchema, "Products");
